4112e4a9d05ba9c01876fdce2344c4182b7b5a73
Eugene <egeis@localhost.localdomain>
2019-01-27 15:04:07 -0500

    Dissertation begins

--- FitCheck.R
+++ FitCheck.R
@@ -1,8 +1,11 @@
+
+
+
 setwd("~/ParSAEM/SAEM_IRT")
 source("CreateSimulationStructure.R")
 
-d = "S2"
-r = 1
+d = "S9"
+r = 3
 
 simdir<-paste0(gen.dir,"/",d)
 SimList<-readRDS(paste0(simdir,"/",SFileString(sim.list[[d]],gen=TRUE),"_",r,".rds"))
@@ -10,23 +13,93 @@ print(SimList$gen.list$gen.xi)
 
 fitdir<-paste0(fit.dir,"/",d)
 FitList<-readRDS(paste0(fitdir,"/",SFileString(sim.list[[d]],gen=FALSE,r = r),".rds"))
+
 if (d=="S2") {
-  plot(FitList$XI[,2],FitList$xi[,2],pch=16,col=4)
+  par(mfrow=c(2,3))
+  plot(FitList$XI[,2],FitList$xi[,2],pch=16,col=4,main="blue=B, red=A, green=C")
   points(FitList$XI[,1],FitList$xi[,1],pch=16,col=2)
   points(FitList$XI[,3],FitList$xi[,3],pch=16,col=3)
-  plot(FitList$XI[,3],FitList$xi[,3],pch=16,col=3)
-  abline(lm(FitList$xi[,3]~FitList$XI[,3]))
-}
-print(FitList$XI - FitList$xi)
-  plot(density(FitList$XI[,1] - FitList$xi[,1]))
-  for ( i in 2:ncol(FitList$XI)) lines(density(FitList$XI[,i] - FitList$xi[,i]),col=i)
-  if (!is.na(FitList$TAU[1])) {
-    print(FitList$TAU - FitList$tau)
-    plot(density(as.vector(FitList$TAU) - as.vector(FitList$tau)))
+  plot(FitList$XI[,3],FitList$xi[,3],pch=16,col=3,main="Guessing with linear fit")
+  Clm<-lm(FitList$xi[,3]~FitList$XI[,3])
+  abline(Clm)
+  text(0.15,0.15,paste0(paste0(coef(Clm),collapse=" + ")," * x"))
+  plot(density(FitList$xi[,1] - FitList$XI[,1]),main="A",col=2)
+  plot(density(FitList$xi[,2] - FitList$XI[,2]),main="B",col=4)
+  plot(density(FitList$xi[,3] - FitList$XI[,3]),main="C",col=3)
+  plot(density(FitList$THETA-FitList$That[,1]),main="Theta")
+} else {
+  AA<-ncol(FitList$XI)
+  KK<-ifelse(with(FitList,exists("TAU")),ncol(FitList$TAU),0)
+  #  sim.list$S3
+  par(mfrow=c(5,ceiling((AA+KK+2)/5)),mar=c(3,2,0.5,0.5))
+  for (i in 1:(AA-1)) {
+    Ah<-sign(sum(FitList$xi[,i]))*FitList$xi[,i]
+    plot(FitList$XI[,i],Ah,pch=16,col=1+i,main=paste0("Slope ",i))
+    Clm<-lm(Ah~FitList$XI[,i])
+    abline(Clm)
+    text(0.2+(i==1)*0.8,0.6,paste0(paste0(format(coef(Clm),digits = 3),collapse="+"),"*x"))
   }
+  Ah<-sign(sum(FitList$xi[,1]))*FitList$xi[,1]
+  plot(density(Ah - FitList$XI[,1]),main="A",col=2)
+  if (AA>2) {
+    for (i in 2:(AA-1)) {
+      Ah<-sign(sum(FitList$xi[,i]))*FitList$xi[,i]
+      lines(density(Ah - FitList$XI[,i]),col=1+i) 
+    }
+  }
+  plot(FitList$XI[,AA],FitList$xi[,AA],pch=16,col=4,main="Intercepts")
+  Clm<-lm(FitList$xi[,AA]~FitList$XI[,AA])
+  abline(Clm)
+  text(0.5,-0.5,paste0(paste0(format(coef(Clm),digits = 3),collapse="+"),"*x"))
+  plot(density(FitList$xi[,AA] - FitList$XI[,AA]),main="Intercepts",col=4)
+  if (with(FitList,exists("TAU"))) {
+    for (i in 1:KK) {
+      plot(FitList$TAU[,i],FitList$tau[,i],pch=16,col=1+i,main=paste0("Offset ",i))
+      Clm<-lm(FitList$tau[,i]~FitList$TAU[,i])
+      abline(Clm)
+      text(0+(i-1)*0.5,-1+(i-1)*0.5,paste0(paste0(format(coef(Clm),digits = 3),collapse="+"),"*x"))
+    }
+    plot(density(FitList$tau[,1] - FitList$TAU[,1]),main="Offset",col=2)
+    for (i in 2:KK) {
+      lines(density(FitList$tau[,i] - FitList$TAU[,i]),col=1+i) 
+    }
+  }
+}
 
-
-plot(FitList$Iterations$Ait[1,1,],type="n")
-lines(FitList$Iterations$Ait[1,1,])
-acf(FitList$Iterations$Bit[1,])
-eigen(FitList$EZZ)
+delta<-FitList$XI[,1] - FitList$xi[,1]
+tests<-cbind(FitList$XI[,1],rankY(FitList$XI[,1]),delta,rankY(-abs(delta)))
+colnames(tests)<-c("A1","A1rank","dA1","dA1rank")
+if (ncol(FitList$XI)>2 && d!="S2") {
+  for (i in 2:(ncol(FitList$XI)-1)) {
+    delta<-FitList$XI[,i] - FitList$xi[,i]
+    tests<-cbind(tests,FitList$XI[,i],rankY(FitList$XI[,i]),delta,rankY(-abs(delta)))
+    colnames(tests)[ncol(tests)-3:0]<-sprintf(fmt = c("A%d","A%drank","dA%d","dA%drank"),i)
+  } 
+}
+if (d!="S2") {
+  delta<-FitList$XI[,ncol(FitList$XI)] - FitList$xi[,ncol(FitList$XI)]
+  tests<-cbind(tests,FitList$XI[,ncol(FitList$XI)],rankY(FitList$XI[,ncol(FitList$XI)]),delta,rankY(-abs(delta)))
+  colnames(tests)[ncol(tests)-3:0]<-sprintf(fmt = c("B%d","B%drank","dB%d","dB%drank"),i)
+} else {
+  delta<-FitList$XI[,ncol(FitList$XI)-1] - FitList$xi[,ncol(FitList$XI)-1]
+  tests<-cbind(tests,FitList$XI[,ncol(FitList$XI)-1],rankY(FitList$XI[,ncol(FitList$XI)-1]),delta,rankY(-abs(delta)))
+  colnames(tests)[ncol(tests)-3:0]<-sprintf(fmt = c("B%d","B%drank","dB%d","dB%drank"),i)
+  delta<-FitList$XI[,ncol(FitList$XI)] - FitList$xi[,ncol(FitList$XI)]
+  tests<-cbind(tests,FitList$XI[,ncol(FitList$XI)],rankY(FitList$XI[,ncol(FitList$XI)]),delta,rankY(-abs(delta)))
+  colnames(tests)[ncol(tests)-3:0]<-sprintf(fmt = c("C%d","C%drank","dC%d","dC%drank"),i)
+  
+}
+print(tests)
+# apply(FitList$XI - FitList$xi,2,function(x) cbind(order(-abs(x)),sort(x,decreasing = T)))
+# 
+# for ( i in 2:ncol(FitList$XI)) lines(density(FitList$XI[,i] - FitList$xi[,i]),col=i)
+# if (exists("FitList$TAU") && !is.na(FitList$TAU[1])) {
+#   print(FitList$TAU - FitList$tau)
+#   plot(density(as.vector(FitList$TAU) - as.vector(FitList$tau)))
+# }
+# 
+# 
+# plot(FitList$Iterations$Ait[1,1,],type="n")
+# lines(FitList$Iterations$Ait[1,1,])
+# acf(FitList$Iterations$Bit[1,])
+# eigen(FitList$EZZ)
---
+++ ThesisPlots/IRT_Didactic.R
@@ -0,0 +1,116 @@
+
+par(mfrow=c(2,2))
+
+pnts1<-list(y=c(0.04+0.025*1:5+rnorm(5,0.05,0.025),0.7+0.04*1:5+rnorm(5,0.05,0.03)),
+           x=c(0.05+0.1*0:9))
+pnts1$theta=qnorm(pnts1$x)
+
+plot(pnts1$x,pnts1$y,xlim=c(0,1),ylim=c(0,1),xlab="score deciles",ylab="P(Y=1|score decile)",main="High Discrimination, Mean Difficulty")
+lines(pnts1$x,pnts1$y)
+
+
+pnts2<-list(y=c(0.45+0.04*1:10+rnorm(10,0.05,0.02)),
+            x=c(0.05+0.1*0:9))
+pnts2$theta=qnorm(pnts2$x)
+
+plot(pnts2$x,pnts2$y,xlim=c(0,1),ylim=c(0,1),xlab="score deciles",ylab="P(Y=1|score decile)",main="Low Discrimination, Low Difficulty")
+lines(pnts2$x,pnts2$y)
+
+
+pnts3<-list(y=c(0.05+0.02*1:8+rnorm(8,0.05,0.025),0.8+0.04*1:2+rnorm(2,0.05,0.03)),
+            x=c(0.05+0.1*0:9))
+pnts3$theta=qnorm(pnts3$x)
+
+plot(pnts3$x,pnts3$y,xlim=c(0,1),ylim=c(0,1),xlab="score deciles",ylab="P(Y=1|score decile)",main="High Discrimination, High Difficulty")
+lines(pnts3$x,pnts3$y)
+
+pnts4<-list(y=c(0.45+0.02*1:10+rnorm(10,0.02,0.06)),
+            x=c(0.05+0.1*0:9))
+pnts4$theta=qnorm(pnts4$x)
+
+plot(pnts4$x,pnts4$y,xlim=c(0,1),ylim=c(0,1),xlab="score deciles",ylab="P(Y=1|score decile)",main="Uninformative")
+lines(pnts4$x,pnts4$y)
+
+
+irt3fit<-function(thet,gam=0.1,alph=1,bet=0) {
+  gam+(1-gam)*1/(1+exp(-1.7*alph*(thet-bet))) 
+}
+ogive3fit<-function(thet,gam=0.1,alph=1,bet=0) {
+  gam+(1-gam)*pnorm(alph*thet-bet)
+}
+
+theta<-qnorm(0.005+0:99*0.01)
+
+fit1<-nls(formula=y~irt3fit(thet,gam,alph,bet),data=data.frame(y=pnts1$y,thet=pnts1$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts1$theta,pnts1$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="IRT-3PL: High Discrimination, Mean Difficulty")
+lines(pnts1$theta,pnts1$y)
+lines(theta,predict(fit1,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit1$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit1$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit1$m$getPars()[1],digits=2),')))')))
+
+fit2<-nls(formula=y~irt3fit(thet,gam,alph,bet),data=data.frame(y=pnts2$y,thet=pnts2$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts2$theta,pnts2$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="IRT-3PL: Low Discrimination, Low Difficulty")
+lines(pnts2$theta,pnts2$y)
+lines(theta,predict(fit2,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit2$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit2$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit2$m$getPars()[1],digits=2),')))')))
+
+fit3<-nls(formula=y~irt3fit(thet,gam,alph,bet),data=data.frame(y=pnts3$y,thet=pnts3$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts3$theta,pnts3$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="IRT-3PL: High Discrimination, High Difficulty")
+lines(pnts3$theta,pnts3$y)
+lines(theta,predict(fit3,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit3$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit3$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit3$m$getPars()[1],digits=2),')))')))
+
+fit4<-nls(formula=y~irt3fit(thet,gam,alph,bet),data=data.frame(y=pnts4$y,thet=pnts4$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts4$theta,pnts4$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="IRT-3PL: Uninformative")
+lines(pnts4$theta,pnts4$y)
+lines(theta,predict(fit4,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit4$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit4$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit4$m$getPars()[1],digits=2),')))')))
+
+############ Ogive fits
+
+fit1<-nls(formula=y~ogive3fit(thet,gam,alph,bet),data=data.frame(y=pnts1$y,thet=pnts1$theta),start = list(gam=0.0,alph=2,bet=0))
+
+plot(pnts1$theta,pnts1$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="3PNO: High Discrimination, Mean Difficulty")
+lines(pnts1$theta,pnts1$y)
+lines(theta,predict(fit1,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit1$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit1$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit1$m$getPars()[1],digits=2),')))')))
+
+fit2<-nls(formula=y~ogive3fit(thet,gam,alph,bet),data=data.frame(y=pnts2$y,thet=pnts2$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts2$theta,pnts2$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="3PNO: Low Discrimination, Low Difficulty")
+lines(pnts2$theta,pnts2$y)
+lines(theta,predict(fit2,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit2$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit2$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit2$m$getPars()[1],digits=2),')))')))
+
+fit3<-nls(formula=y~ogive3fit(thet,gam,alph,bet),data=data.frame(y=pnts3$y,thet=pnts3$theta),start = list(gam=0.1,alph=1,bet=0))
+
+plot(pnts3$theta,pnts3$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="3PNO: High Discrimination, High Difficulty")
+lines(pnts3$theta,pnts3$y)
+lines(theta,predict(fit3,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit3$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit3$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit3$m$getPars()[1],digits=2),')))')))
+
+fit4<-nls(formula=y~ogive3fit(thet,gam,alph,bet),data=data.frame(y=pnts4$y,thet=pnts4$theta),start = list(gam=0.5,alph=0.2,bet=0))
+
+plot(pnts4$theta,pnts4$y,xlim=c(-3,3),ylim=c(0,1),xlab="score deciles as Z-scores",ylab="P(Y=1|Z-score)",main="3PNO: Uninformative")
+lines(pnts4$theta,pnts4$y)
+lines(theta,predict(fit4,newdata = data.frame(thet=theta)),lwd=2,col=2,lty=3)
+eval(parse(text=paste0('text(2,0.55,expression(paste(hat(alpha)," = ",',format(fit4$m$getPars()[2],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.4,expression(paste(hat(beta)," = ",',format(fit4$m$getPars()[3],digits=2),')))')))
+eval(parse(text=paste0('text(2,0.25,expression(paste(hat(gamma)," = ",',format(fit4$m$getPars()[1],digits=2),')))')))
